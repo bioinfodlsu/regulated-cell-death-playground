07b2a4d6f8ab0001342c5f6ee01416fc5a36a66d
memgonzales <gonzales.markedward@gmail.com>
2024-09-30 16:40:40 +0800

    Add script for installing dependencies

---
+++ install-dependencies.r
@@ -0,0 +1,8 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("TCGAbiolinks")
+BiocManager::install("RNAseqQC")
+BiocManager::install("DESeq2")
+BiocManager::install("ensembldb")
+BiocManager::install("vsn")
